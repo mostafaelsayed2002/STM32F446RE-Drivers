58ff86f381999148c5560e8fbec7d6969d65579c
Mostafa Elsayed <mostafa.elsayed.2002@gmail.com>
2024-07-16 11:05:10 +0300

    refactor: Update SYSCFG driver to support setting EXTI port for specific line

--- MCAL/SYSCFG/Inc/SYSCFG_interface.h
+++ MCAL/SYSCFG/Inc/SYSCFG_interface.h
@@ -2,6 +2,48 @@
 #ifndef SYSCFG_INTERFACE_H
 #define SYSCFG_INTERFACE_H
 
-void SYSCFG_voidSetExtiPort(EXTI_Line_t line, Port_t port);
+#include "EXTI_interface.h"
+#include "GPIO_interface.h"
+
+typedef enum
+{
+    EXTI0,
+    EXTI1,
+    EXTI2,
+    EXTI3,
+    EXTI4,
+    EXTI5,
+    EXTI6,
+    EXTI7,
+    EXTI8,
+    EXTI9,
+    EXTI10,
+    EXTI11,
+    EXTI12,
+    EXTI13,
+    EXTI14,
+    EXTI15
+} SYSCFG_Line_t;
+
+typedef enum
+{
+    PORTA = 0,
+    PORTB,
+    PORTC,
+    PORTD,
+    PORTE,
+    PORTF,
+    PORTG,
+    PORTH
+} SYSCFG_Port_t;
+
+/**
+ * @fn     SYSCFG_u8SetExtiPort
+ * @brief  Set the EXTI port for a specific line
+ * @param[in]   line: The EXTI line
+ * @param[in]   port: The EXTI port
+ * @retval Error status
+ */
+uint8_t SYSCFG_u8SetExtiPort(SYSCFG_Line_t line, SYSCFG_Port_t port);
 
 #endif
--- MCAL/SYSCFG/Src/SYSCFG_program.c
+++ MCAL/SYSCFG/Src/SYSCFG_program.c
@@ -6,15 +6,25 @@
  */
 
 #include <stdint.h>
+#include "ErrType.h"
 #include "STM32F466xx.h"
 #include "SYSCFG_interface.h"
-#include "EXTI_interface.h"
-#include "GPIO_interface.h"
 
-void SYSCFG_voidSetExtiPort(EXTI_Line_t line, Port_t port)
+uint8_t SYSCFG_u8SetExtiPort(SYSCFG_Line_t line, SYSCFG_Port_t port)
 {
-    uint8_t regNum = line / 4;
-    uint8_t pinNum = line % 4;
-    SYSCFG->EXTICR[regNum] &= ~(0xF << (pinNum * 4));
-    SYSCFG->EXTICR[regNum] |= (port << (pinNum * 4));
+    uint8_t Local_u8ErrorState = OK;
+
+    if (line <= EXTI15 && port <= PORTH)
+    {
+        uint8_t regNum = line / 4;
+        uint8_t pinNum = line % 4;
+        SYSCFG->EXTICR[regNum] &= ~(0xF << (pinNum * 4));
+        SYSCFG->EXTICR[regNum] |= (port << (pinNum * 4));
+    }
+    else
+    {
+        Local_u8ErrorState = NOK;
+    }
+
+    return Local_u8ErrorState;
 }
